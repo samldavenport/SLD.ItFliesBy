e08b4451572c868f4dd787e3657fdbdd8615ee68
thereclaimer <sam.davenport@thereclaimergame.com>
2026-05-18 13:28:34 -0500

    initial files

---
+++ src/win32/win32-main.cpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Windows.h>
+
+#include "sld.hpp"
+
+int WINAPI
+wWinMain(
+    HINSTANCE h_instance,
+    HINSTANCE h_prevInstance,
+    PWSTR     p_cmd_line,
+    int       n_cmd_show) {
+
+    return(0);
+}
