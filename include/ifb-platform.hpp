f94d64e458163dca229046007cc367868d6978de
Sam Davenport <sam.davenport@thereclaimergame.com>
2026-05-19 08:39:09 -0500

    window events

--- include/ifb-platform.hpp
+++ include/ifb-platform.hpp
@@ -7,10 +7,34 @@ using namespace sld;
 
 namespace ifb {
 
-    struct window_config;
+    typedef u32 window_event_flags;
 
-    void platform_window_open(window_config* cfg);
-    
+    struct window_context;
+    struct window_config;
+    struct window_position;
+    struct window_size;
+
+    void window_open  (window_context* ctx, const window_config* cfg);
+    void window_close (window_context* ctx);
+
+    struct window_context {
+        window_event_flags event_flags;
+    };
+
+    enum window_event_flag_e {
+        window_event_flag_e_none              = 0;
+        window_event_flag_e_close             = bit_value(1),
+        window_event_flag_e_minimize          = bit_value(2),
+        window_event_flag_e_position_update   = bit_value(3),
+        window_event_flag_e_size_update       = bit_value(4),
+        window_event_flag_e_key_down          = bit_value(5),
+        window_event_flag_e_key_up            = bit_value(6),
+        window_event_flag_e_mouse_click_left  = bit_value(7),
+        window_event_flag_e_mouse_click_right = bit_value(8),
+        window_event_flag_e_mouse_move        = bit_value(9),
+        window_event_flag_e_mouse_scroll      = bit_value(10)
+    };
+
 };
 
 #endif //IFB_PLATFORM_HPP
