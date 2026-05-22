55c1421d0c3303f79365348120ccfdab0657589b
thereclaimer <sam.davenport@thereclaimergame.com>
2026-05-22 17:10:39 -0500

    set the viewport and clear color

--- src/win32/win32-graphics.cpp
+++ src/win32/win32-graphics.cpp
@@ -9,15 +9,17 @@ namespace ifb {
         void) {
 
         color_rgba_u32 clear_color;
-        clear_color.hex = 0; 
+        clear_color.hex = 0x282828FF;
         win32_window& window = win32_window_instance();
 
 
         RECT window_rect;
         GetWindowRect(window.handle, &window_rect);
 
-        gl_context_init_win32           (&window.context, window.handle);
-        gl_context_set_clear_color_rgba (&window.context, clear_color);
-        gl_context_update_viewport      (&window.context, 0,0, window_rect.right, window_rect.bottom);
+        gl_context_init_win32             (&window.context, window.handle);
+        gl_context_enable_depth_buffering (&window.context);
+        gl_context_enable_smoothing       (&window.context);
+        gl_context_set_clear_color_rgba   (&window.context, clear_color);
+        gl_context_update_viewport        (&window.context, 0,0, window_rect.right, window_rect.bottom);
     }
 };
