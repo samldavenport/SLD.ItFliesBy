839cf5db359ed57c6b1942cb7be4785cf1599b88
thereclaimer <sam.davenport@thereclaimergame.com>
2026-06-27 13:11:31 -0500

    hashing out the arena

---
+++ src/memory/arena.cpp
@@ -0,0 +1,187 @@
+#pragma once
+
+#include "memory.hpp"
+
+namespace ifb {
+
+    //--------------------------------------------------------------------
+    // INLINE METHOD DECLARATIONS
+    //--------------------------------------------------------------------
+
+    inline void arena_allocator_validate (void);
+    inline void arena_validate           (const arena* a);
+
+    //--------------------------------------------------------------------
+    // INTERNAL METHOD DEFINITIONS
+    //--------------------------------------------------------------------
+
+    IFB_INTERNAL arena*
+    arena_alloc(
+        void) {
+
+        arena_allocator_validate();
+
+        arena_allocator* alctr = _memory_manager->arena_alctr;
+        arena*           a     = alctr->list.free;
+
+        if (a != NULL) {
+
+            assert(a->prev == NULL);
+
+            // get the next free and used
+            arena* free_next = a->next;
+            arena* used_next = alctr->list.used;
+
+            // update the free list
+            free_next->prev = NULL;
+            alctr->list.free = free_next;
+
+            // update the used list
+            if (used_next) {
+                assert(used_next->prev == NULL);
+                used_next->prev = a;
+            }
+            a->next = used_next;
+            alctr->list.used = a; 
+        }
+
+        return(a);
+    }
+
+    IFB_INTERNAL void
+    arena_free(
+        arena* a) {
+
+        arena_allocator_validate();
+        arena_validate(a);
+
+        arena_allocator* alctr     = _memory_manager->arena_alctr;
+        arena*           used_next = a->next;
+        arena*           used_prev = a->prev;
+        arena*           free_next = alctr->list.free;
+
+        // update the used list
+        if (used_next)             used_next->prev  = used_prev;
+        if (used_prev)             used_prev->next  = used_next;
+        if (a == alctr->list.used) alctr->list.used = used_next;
+
+        // update the free list
+        if (free_next) {
+            assert(free_next->prev == NULL);
+            free_next->prev = a;
+        }
+        a->prev          = NULL;
+        a->next          = free_next;
+        alctr->list.free = a;
+    }
+
+    IFB_INTERNAL void
+    arena_reset(
+        arena* a) {
+
+        arena_allocator_validate();
+        arena_validate(a);
+
+        a->position = 0;
+        a->save     = 0;
+    }
+
+    IFB_INTERNAL u32
+    arena_save(
+        arena* a) {
+
+        arena_allocator_validate();
+        arena_validate(a);
+
+        a->save = a->position;
+        return(a->save);
+    }
+
+    IFB_INTERNAL void*
+    arena_push(
+        arena*    a,
+        const u32 size) {
+
+        arena_allocator_validate();
+        arena_validate(a);
+        
+        const arena_allocator* alctr           = _memory_manager->arena_alctr;
+        const u32              space_remaining = (alctr->arena_size - a->position); 
+
+        // make sure we have space        
+        if (size > space_remaining) {
+            return(NULL);
+        }
+
+        // calculate pointer
+        const u32  offset = (a->position + sizeof(arena));
+        const addr start  = (addr)a;
+        void*      mem    = (void*)(start + offset);
+
+        // update the position and return
+        a->position += size;
+        return(mem);
+    }
+
+    IFB_INTERNAL void
+    arena_revert(
+        arena*    a,
+        const u32 save) {
+
+        arena_allocator_validate();
+        arena_validate(a);
+
+        assert(a->save == save);
+
+        a->position = a->save;
+        a->save     = 0;
+    }
+
+    //--------------------------------------------------------------------
+    // INLINE METHOD DEFINITIONS
+    //--------------------------------------------------------------------
+
+    inline void
+    arena_allocator_validate(
+        void) {
+
+        assert(_memory_manager);
+
+        arena_allocator* alctr = _memory_manager->arena_alctr;
+        assert(
+            alctr              != NULL &&
+            alctr->mem.address != 0    &&
+            alctr->mem.size    != 0    &&
+            alctr->arena_count != 0    &&
+            alctr->arena_size  != 0    &&
+            (
+                alctr->list.free != NULL ||
+                alctr->list.used != NULL
+            )
+        );
+    }
+
+    inline void
+    arena_validate(
+        const arena* a) {
+        
+        assert(
+            _memory_manager != NULL &&
+            a               != NULL &&
+            a->alctr        == _memory_manager->arena_alctr;
+        );
+
+        const arena_allocator* arena_alctr  = a->alctr;
+        const u32              arena_offset = (a->id * arena_alctr->arena_size); 
+        const void*            arena_vptr   = (void*)(arena_alctr->mem.address + arena_offset); 
+
+        assert(
+            a           == (arena*)arena_vptr       &&
+            a->id       <  arena_alctr->arena_count &&
+            a->save     <= a->position              &&
+            a->position <  alctr->arena_size
+        );
+    }
+
+
+};
---
+++ src/memory/memory.hpp
@@ -0,0 +1,60 @@
+#ifndef MEMORY_HPP
+#define MEMORY_HPP
+
+#include "ifb.hpp"
+
+namespace ifb {
+
+    //--------------------------------------------------------------------
+    // STRUCTURED TYPES
+    //--------------------------------------------------------------------
+
+    struct memory_manager;
+    struct arena_allocator;
+    struct stack_allocator;
+    struct block_allocator;
+    struct arena;
+
+    //--------------------------------------------------------------------
+    // METHODS
+    //--------------------------------------------------------------------
+
+    IFB_INTERNAL u32    memory_manager_memory_requirement (const u32 allocator_count);
+    IFB_INTERNAL void   memory_manager_memory_init        (const u32 allocator_count, memory& mem_stack);
+    IFB_INTERNAL void   memory_manager_startup            (memory& mem_reserved);
+    IFB_INTERNAL void   memory_manager_shutdown           (void);
+
+    IFB_INTERNAL arena* arena_alloc  (void);
+    IFB_INTERNAL void   arena_free   (arena* a);
+    IFB_INTERNAL void   arena_reset  (arena* a);
+    IFB_INTERNAL u32    arena_save   (arena* a);
+    IFB_INTERNAL void*  arena_push   (arena* a, const u32 size);
+    IFB_INTERNAL void   arena_revert (arena* a, const u32 save);
+
+    //--------------------------------------------------------------------
+    // TYPE DEFINITIONS
+    //--------------------------------------------------------------------
+
+    struct arena_allocator {
+        memory mem;
+        u32    arena_size;
+        u32    arena_count;
+        struct {
+            arena* free;
+            arena* used;
+        } list;
+    };
+
+    struct arena {
+        arena_allocator* alctr;
+        arena*           next;
+        arena*           prev;
+        u32              id;
+        u32              position;
+        u32              save;
+    };
+
+    struct memory_manager {
+        arena_allocator* arena_alctr;
+    } static * _memory_manager;
+};
