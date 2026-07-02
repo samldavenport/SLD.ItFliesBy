b51ec662803c14b7f91cdb588c19ffbd7957ad92
thereclaimer <sam.davenport@thereclaimergame.com>
2026-07-01 21:35:34 -0500

    component tables

--- src/components/components.hpp
+++ src/components/components.hpp
@@ -2,14 +2,42 @@
 #define COMPONENTS_HPP
 
 #include "ifb.hpp"
-
+#include "entities.hpp"
 namespace ifb {
 
     struct component_manager;
+    struct component_table_position;
+    struct component_table_color;
 
     u32                component_manager_memory_size (void);
     component_manager* component_manager_create      (memory& mem_stack);
     void               component_manager_startup     (memory& mem_res);
+
+
+    struct component_manager {
+        struct  {
+            component_table_position* position; 
+            component_table_color*    color; 
+        } tables;
+    };
+
+    struct component_table_position {
+        entity_sparse_set* sparse_entities;
+        struct {
+            entity_id* id
+            f32*       x;
+            f32*       y;
+            f32*       z;
+        } sparse_data;
+    };
+
+    struct component_table_color {
+        entity_sparse_set* sparse_entities;
+        struct {
+            entity_id* id;
+            u32*       rgba_hex;
+        } sparse_data;
+    };
 
 };
 
