5343245d010a75648c6d2630cd93dc37b89581bd
thereclaimer <sam.davenport@thereclaimergame.com>
2026-07-08 20:52:17 -0500

    initial test function

---
+++ src/quads/quad-testing.cpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "quad.hpp"
+
+namespace ifb {
+
+    static quad_list* _quad_list;
+    static arena*     _arena;
+
+    inline void create_100_quads(void);
+
+    IFB_INTERNAL void
+    quad_tests(void) {
+
+        _arena = arena_alloc();
+        assert(a);
+
+
+
+        for (
+
+        )
+    }
+
+    inline void
+    create_100_quads(
+        void) {
+
+        
+    }
+};
